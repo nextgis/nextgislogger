d16e1010d0d39a5b6e9beb88aea34fc28b42b169
NikitaFeodonit <nikitafeodonit@yandex.com>
2014-08-21 08:10:13 +0200

    CSV-file writer for marks.

--- src/com/nextgis/gsm_logger/MainActivity.java
+++ src/com/nextgis/gsm_logger/MainActivity.java
@@ -5,23 +5,64 @@ import android.app.ActivityManager;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Environment;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
+import android.widget.Toast;
+import au.com.bytecode.opencsv.CSV;
+import au.com.bytecode.opencsv.CSVWriteProc;
+import au.com.bytecode.opencsv.CSVWriter;
+
+import java.io.*;
 
 public class MainActivity extends Activity {
 
     private Intent mIntentService;
+
+    private static final CSV mCSVMarkers = CSV
+            // TODO: It is draft
+            .separator(';')
+            .quote('\'')
+            .skipLines(1)
+            .charset("UTF-8")
+            .create();
+
+    private String mCSVFileName = "time_marks.csv";
 
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        setContentView(R.layout.main_activity);
+        if (!Environment.getExternalStorageState().equals(Environment.MEDIA_MOUNTED)) {
+            Toast.makeText(this, R.string.ext_media_unmounted, Toast.LENGTH_LONG).show();
+            finish();
+        }
+
+        mCSVFileName = getExternalFilesDir(Environment.DIRECTORY_DOWNLOADS).getAbsolutePath() +
+                File.separator + mCSVFileName;
+
+        try {
+            File csvFile = new File(mCSVFileName);
+            if (!csvFile.exists()) {
+                mCSVMarkers.writeAndClose(new FileOutputStream(csvFile, true), new CSVWriteProc() {
+                    public void process(CSVWriter out) {
+                        // TODO: It is draft
+                        out.writeNext("Header1", "Header2");
+                    }
+                });
+            }
+        } catch (FileNotFoundException e) {
+            Toast.makeText(this, R.string.file_system_error, Toast.LENGTH_LONG).show();
+            finish();
+        }
+
         mIntentService = new Intent(this, LoggerService.class);
         boolean isServiceRunning = isLoggerServiceRunning();
+
+        setContentView(R.layout.main_activity);
 
         final Button serviceOnOffButton = (Button) findViewById(R.id.btn_service_onoff);
         serviceOnOffButton.setText(getString(isServiceRunning
@@ -52,19 +93,34 @@ public class MainActivity extends Activity {
         markButton.setText(getString(R.string.btn_make_mark));
 
         final EditText markTextEditor = (EditText) findViewById(R.id.mark_text_editor);
+        final Toast errorMsg = Toast.makeText(this, R.string.file_system_error, Toast.LENGTH_LONG);
 
         markButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
 
                 if (markTextEditor.isShown()) {
-                    // TODO: save text
+
+                    try {
+                        mCSVMarkers.writeAndClose(new FileOutputStream(mCSVFileName, true),
+                                new CSVWriteProc() {
+                                    public void process(CSVWriter out) {
+                                        // TODO: It is draft
+                                        out.writeNext(markTextEditor.getText().toString(), "v22");
+                                    }
+                                });
+                    } catch (FileNotFoundException e) {
+                        errorMsg.show();
+                        finish();
+                    }
+
                     markTextEditor.setVisibility(View.GONE);
                     markButton.setText(getString(R.string.btn_make_mark));
+
                 } else {
+                    markTextEditor.setText("");
                     markTextEditor.setVisibility(View.VISIBLE);
                     markButton.setText(getString(R.string.btn_save_mark));
                 }
-
             }
         });
     }
