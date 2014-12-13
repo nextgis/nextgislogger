a1eaeb55c7dcc03ed9a16786e34b04f02ecb2900
Stanislav Petryakov <become.glory@yandex.ru>
2014-12-13 16:55:33 +0300

    Deprecation > in PreferencesActivity fragment implemented

--- src/com/nextgis/logger/PreferencesActivity.java
+++ src/com/nextgis/logger/PreferencesActivity.java
@@ -19,6 +19,7 @@ import android.preference.Preference;
 import android.preference.Preference.OnPreferenceChangeListener;
 import android.preference.Preference.OnPreferenceClickListener;
 import android.preference.PreferenceActivity;
+import android.preference.PreferenceFragment;
 import android.widget.Toast;
 
 public class PreferencesActivity extends PreferenceActivity {
@@ -28,122 +29,133 @@ public class PreferencesActivity extends PreferenceActivity {
 	@Override
 	protected void onCreate(Bundle savedInstance) {
 		super.onCreate(savedInstance);
-		addPreferencesFromResource(R.xml.preferences);
-		final Activity parent = this;
-		
-		SensorManager sm = (SensorManager) this.getSystemService(Context.SENSOR_SERVICE);
+		getFragmentManager().beginTransaction().replace(android.R.id.content, new PreferencesFragment()).commit();
+	}
 
-		if (sm.getDefaultSensor(Sensor.TYPE_LINEAR_ACCELERATION) == null)
-			findPreference(C.PREF_SENSOR_MODE).setEnabled(false);
+	public static class PreferencesFragment extends PreferenceFragment {
+		@SuppressWarnings("deprecation")
+		@Override
+		public void onCreate(Bundle savedInstanceState) {
+			super.onCreate(savedInstanceState);
 
-		if (sm.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD) == null)
-			findPreference(C.PREF_SENSOR_MAG).setEnabled(false);
-		
-		if (sm.getDefaultSensor(Sensor.TYPE_ORIENTATION) == null)
-			findPreference(C.PREF_SENSOR_ORIENT).setEnabled(false);
-		
-		if (sm.getDefaultSensor(Sensor.TYPE_GYROSCOPE) == null)
-			findPreference(C.PREF_SENSOR_GYRO).setEnabled(false);
-		
-		if (android.os.Build.VERSION.SDK_INT < android.os.Build.VERSION_CODES.JELLY_BEAN_MR1)
-			findPreference(C.PREF_USE_API17).setEnabled(false);
+			addPreferencesFromResource(R.xml.preferences);
 
-		EditTextPreference userName = (EditTextPreference) findPreference(C.PREF_USER_NAME);
-		userName.setSummary(userName.getText());
-		userName.setOnPreferenceChangeListener(new OnPreferenceChangeListener() {
-			@Override
-			public boolean onPreferenceChange(Preference preference, Object newValue) {
-				preference.setSummary((String) newValue);
+			final Activity parent = getActivity();
 
-				return true;
-			}
-		});
+			SensorManager sm = (SensorManager) parent.getSystemService(Context.SENSOR_SERVICE);
 
-		IntEditTextPreference periodPreference = (IntEditTextPreference) findPreference(C.PREF_PERIOD_SEC);
-		periodPreference.setSummary(getString(R.string.settings_period_sum) + periodPreference.getPersistedString("1"));
-		periodPreference.setOnPreferenceChangeListener(new OnPreferenceChangeListener() {
-			@Override
-			public boolean onPreferenceChange(Preference preference, Object newValue) {
-				int period;
+			if (sm.getDefaultSensor(Sensor.TYPE_LINEAR_ACCELERATION) == null)
+				findPreference(C.PREF_SENSOR_MODE).setEnabled(false);
 
-				try {
-					period = Integer.parseInt((String) newValue);
-					boolean max = period > maxPeriodSec;
-					boolean min = period < minPeriodSec;
+			if (sm.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD) == null)
+				findPreference(C.PREF_SENSOR_MAG).setEnabled(false);
 
-					if (max)
-						period = maxPeriodSec;
+			if (sm.getDefaultSensor(Sensor.TYPE_ORIENTATION) == null)
+				findPreference(C.PREF_SENSOR_ORIENT).setEnabled(false);
 
-					if (min)
-						period = minPeriodSec;
+			if (sm.getDefaultSensor(Sensor.TYPE_GYROSCOPE) == null)
+				findPreference(C.PREF_SENSOR_GYRO).setEnabled(false);
 
-					((IntEditTextPreference) preference).persistString(Integer.toString(period));
-					preference.setSummary(getString(R.string.settings_period_sum) + period);
+			if (android.os.Build.VERSION.SDK_INT < android.os.Build.VERSION_CODES.JELLY_BEAN_MR1)
+				findPreference(C.PREF_USE_API17).setEnabled(false);
 
-					if (min || max)
-						throw new IllegalArgumentException();
-				} catch (Exception e) {
-					Toast.makeText(getBaseContext(), R.string.settings_period_toast, Toast.LENGTH_LONG).show();
+			EditTextPreference userName = (EditTextPreference) findPreference(C.PREF_USER_NAME);
+			userName.setSummary(userName.getText());
+			userName.setOnPreferenceChangeListener(new OnPreferenceChangeListener() {
+				@Override
+				public boolean onPreferenceChange(Preference preference, Object newValue) {
+					preference.setSummary((String) newValue);
+
+					return true;
 				}
+			});
 
-				return false;
-			}
-		});
+			IntEditTextPreference periodPreference = (IntEditTextPreference) findPreference(C.PREF_PERIOD_SEC);
+			periodPreference.setSummary(getString(R.string.settings_period_sum) + periodPreference.getPersistedString("1"));
+			periodPreference.setOnPreferenceChangeListener(new OnPreferenceChangeListener() {
+				@Override
+				public boolean onPreferenceChange(Preference preference, Object newValue) {
+					int period;
 
-		Preference catPathPreference = findPreference(C.PREF_CAT_PATH);
+					try {
+						period = Integer.parseInt((String) newValue);
+						boolean max = period > maxPeriodSec;
+						boolean min = period < minPeriodSec;
 
-		catPathPreference.setOnPreferenceClickListener(new OnPreferenceClickListener() {
-			@Override
-			public boolean onPreferenceClick(final Preference preference) {
-				SimpleFileDialog FileOpenDialog = new SimpleFileDialog(parent, new SimpleFileDialog.SimpleFileDialogListener() {
-					@Override
-					public void onChosenDir(String chosenDir) {
-						// The code in this function will be executed when the dialog OK button is pushed
-						String info = getString(R.string.error_no_file);
+						if (max)
+							period = maxPeriodSec;
 
-						if (new File(chosenDir).isFile()) {
-							File fromCats = new File(chosenDir);
+						if (min)
+							period = minPeriodSec;
 
-							String internalPath = getFilesDir().getAbsolutePath();
-							File toCats = new File(internalPath + "/" + C.categoriesFile);
+						((IntEditTextPreference) preference).persistString(Integer.toString(period));
+						preference.setSummary(getString(R.string.settings_period_sum) + period);
 
-							try {
-								PrintWriter pw = new PrintWriter(new FileOutputStream(toCats, false));
-								BufferedReader in = new BufferedReader(new FileReader(fromCats));
-
-								String[] split;
-								String line;
-
-								while ((line = in.readLine()) != null) {
-									split = line.split(",");
-
-									if (split.length != 2) {
-										in.close();
-										pw.close();
-										throw new ArrayIndexOutOfBoundsException("Must be two columns splitted by ','!");
-									} else
-										pw.println(line);
-								}
-
-								in.close();
-								pw.close();
-
-								info = getString(R.string.file_loaded) + chosenDir;
-							} catch (IOException e) {
-								info = getString(R.string.fs_error_msg);
-							} catch (ArrayIndexOutOfBoundsException e) {
-								info = getString(R.string.cat_file_structure_error);
-							}
-						}
-
-						Toast.makeText(parent, info, Toast.LENGTH_SHORT).show();
+						if (min || max)
+							throw new IllegalArgumentException();
+					} catch (Exception e) {
+						Toast.makeText(parent, R.string.settings_period_toast, Toast.LENGTH_LONG).show();
 					}
-				});
 
-				FileOpenDialog.chooseFile_or_Dir();
+					return false;
+				}
+			});
 
-				return true;
-			}
-		});
+			Preference catPathPreference = findPreference(C.PREF_CAT_PATH);
+
+			catPathPreference.setOnPreferenceClickListener(new OnPreferenceClickListener() {
+				@Override
+				public boolean onPreferenceClick(final Preference preference) {
+					SimpleFileDialog FileOpenDialog = new SimpleFileDialog(parent, new SimpleFileDialog.SimpleFileDialogListener() {
+						@Override
+						public void onChosenDir(String chosenDir) {
+							// The code in this function will be executed when the dialog OK button is pushed
+							String info = getString(R.string.error_no_file);
+
+							if (new File(chosenDir).isFile()) {
+								File fromCats = new File(chosenDir);
+
+								String internalPath = parent.getFilesDir().getAbsolutePath();
+								File toCats = new File(internalPath + "/" + C.categoriesFile);
+
+								try {
+									PrintWriter pw = new PrintWriter(new FileOutputStream(toCats, false));
+									BufferedReader in = new BufferedReader(new FileReader(fromCats));
+
+									String[] split;
+									String line;
+
+									while ((line = in.readLine()) != null) {
+										split = line.split(",");
+
+										if (split.length != 2) {
+											in.close();
+											pw.close();
+											throw new ArrayIndexOutOfBoundsException("Must be two columns splitted by ','!");
+										} else
+											pw.println(line);
+									}
+
+									in.close();
+									pw.close();
+
+									info = getString(R.string.file_loaded) + chosenDir;
+								} catch (IOException e) {
+									info = getString(R.string.fs_error_msg);
+								} catch (ArrayIndexOutOfBoundsException e) {
+									info = getString(R.string.cat_file_structure_error);
+								}
+							}
+
+							Toast.makeText(parent, info, Toast.LENGTH_SHORT).show();
+						}
+					});
+
+					FileOpenDialog.chooseFile_or_Dir();
+
+					return true;
+				}
+			});
+		}
 	}
 }
